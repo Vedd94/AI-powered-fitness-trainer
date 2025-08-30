b3e6267d3c660baabc7a6073e0ad09ca384ade55
Vedd <veddahale94@gmail.com>
2025-08-30 15:46:13 +0530

    Ved -> pose_estimator_modeul done

---
+++ pose_estimator_module.py
@@ -0,0 +1,67 @@
+import cv2
+import mediapipe as mp
+
+class poseDetector():
+    def __init__(self, mode=False, upBody=False, smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(
+            static_image_mode=self.mode,
+            model_complexity=1 if not self.upBody else 0, 
+            smooth_landmarks=self.smooth,
+            enable_segmentation=False,
+            min_detection_confidence=self.detectionCon,
+            min_tracking_confidence=self.trackCon
+        )
+
+        self.results = None   # 👈 initialize results
+
+    
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)   # 👈 save results to self
+
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        
+        return img
+    
+    def getPosition(self, img, draw=True):
+        lmList = []
+
+        if self.results and self.results.pose_landmarks:  
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        
+        return lmList
+    
+
+import time
+def main():
+    cap = cv2.VideoCapture('skipping.mp4')
+    pTime = 0
+    detector = poseDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.getPosition(img, draw=False)
+        cTime = time.time
+        
+        cv2.imshow("WebCam", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
